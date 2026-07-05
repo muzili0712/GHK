52caf9ee174aee485f2a1babf4171a0077a22c21
FGBLH <pengli08211@gmail.com>
2026-07-05 18:51:27 +0800

    Update chaturbate.py

--- py/chaturbate.py
+++ py/chaturbate.py
@@ -1,54 +1,109 @@
+from __future__ import unicode_literals
+
 import re
-import uuid
 
-from streamlink.plugin import Plugin
-from streamlink.plugin.api import validate
-from streamlink.stream import HLSStream
-
-API_HLS = "https://chaturbate.com/get_edge_hls_url_ajax/"
-
-_url_re = re.compile(r"https?://(\w+\.)?chaturbate\.com/(?P<username>\w+)")
-
-_post_schema = validate.Schema(
-    {
-        "url": validate.text,
-        "room_status": validate.text,
-        "success": int
-    }
+from .common import InfoExtractor
+from ..utils import (
+    ExtractorError,
+    lowercase_escape,
+    url_or_none,
 )
 
 
-class Chaturbate(Plugin):
-    @classmethod
-    def can_handle_url(cls, url):
-        return _url_re.match(url)
+class ChaturbateIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:[^/]+\.)?chaturbate\.com/(?:fullvideo/?\?.*?\bb=)?(?P<id>[^/?&#]+)'
+    _TESTS = [{
+        'url': 'https://www.chaturbate.com/siswet19/',
+        'info_dict': {
+            'id': 'siswet19',
+            'ext': 'mp4',
+            'title': 're:^siswet19 [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$',
+            'age_limit': 18,
+            'is_live': True,
+        },
+        'params': {
+            'skip_download': True,
+        },
+        'skip': 'Room is offline',
+    }, {
+        'url': 'https://chaturbate.com/fullvideo/?b=caylin',
+        'only_matching': True,
+    }, {
+        'url': 'https://en.chaturbate.com/siswet19/',
+        'only_matching': True,
+    }]
 
-    def _get_streams(self):
-        match = _url_re.match(self.url)
-        username = match.group("username")
+    _ROOM_OFFLINE = 'Room is currently offline'
 
-        CSRFToken = str(uuid.uuid4().hex.upper()[0:32])
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
 
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "X-CSRFToken": CSRFToken,
-            "X-Requested-With": "XMLHttpRequest",
-            "Referer": self.url,
+        webpage = self._download_webpage(
+            'https://chaturbate.com/%s/' % video_id, video_id,
+            headers=self.geo_verification_headers())
+
+        found_m3u8_urls = []
+
+        data = self._parse_json(
+            self._search_regex(
+                r'initialRoomDossier\s*=\s*(["\'])(?P<value>(?:(?!\1).)+)\1',
+                webpage, 'data', default='{}', group='value'),
+            video_id, transform_source=lowercase_escape, fatal=False)
+        if data:
+            m3u8_url = url_or_none(data.get('hls_source'))
+            if m3u8_url:
+                found_m3u8_urls.append(m3u8_url)
+
+        if not found_m3u8_urls:
+            for m in re.finditer(
+                    r'(\\u002[27])(?P<url>http.+?\.m3u8.*?)\1', webpage):
+                found_m3u8_urls.append(lowercase_escape(m.group('url')))
+
+        if not found_m3u8_urls:
+            for m in re.finditer(
+                    r'(["\'])(?P<url>http.+?\.m3u8.*?)\1', webpage):
+                found_m3u8_urls.append(m.group('url'))
+
+        m3u8_urls = []
+        for found_m3u8_url in found_m3u8_urls:
+            m3u8_fast_url, m3u8_no_fast_url = found_m3u8_url, found_m3u8_url.replace('_fast', '')
+            for m3u8_url in (m3u8_fast_url, m3u8_no_fast_url):
+                if m3u8_url not in m3u8_urls:
+                    m3u8_urls.append(m3u8_url)
+
+        if not m3u8_urls:
+            error = self._search_regex(
+                [r'<span[^>]+class=(["\'])desc_span\1[^>]*>(?P<error>[^<]+)</span>',
+                 r'<div[^>]+id=(["\'])defchat\1[^>]*>\s*<p><strong>(?P<error>[^<]+)<'],
+                webpage, 'error', group='error', default=None)
+            if not error:
+                if any(p in webpage for p in (
+                        self._ROOM_OFFLINE, 'offline_tipping', 'tip_offline')):
+                    error = self._ROOM_OFFLINE
+            if error:
+                raise ExtractorError(error, expected=True)
+            raise ExtractorError('Unable to find stream URL')
+
+        formats = []
+        for m3u8_url in m3u8_urls:
+            for known_id in ('fast', 'slow'):
+                if '_%s' % known_id in m3u8_url:
+                    m3u8_id = known_id
+                    break
+            else:
+                m3u8_id = None
+            formats.extend(self._extract_m3u8_formats(
+                m3u8_url, video_id, ext='mp4',
+                # ffmpeg skips segments for fast m3u8
+                preference=-10 if m3u8_id == 'fast' else None,
+                m3u8_id=m3u8_id, fatal=False, live=True))
+        self._sort_formats(formats)
+
+        return {
+            'id': video_id,
+            'title': self._live_title(video_id),
+            'thumbnail': 'https://roomimg.stream.highwebmedia.com/ri/%s.jpg' % video_id,
+            'age_limit': self._rta_search(webpage),
+            'is_live': True,
+            'formats': formats,
         }
-
-        cookies = {
-            "csrftoken": CSRFToken,
-        }
-
-        post_data = "room_slug={0}&bandwidth=high".format(username)
-
-        res = self.session.http.post(API_HLS, headers=headers, cookies=cookies, data=post_data)
-        data = self.session.http.json(res, schema=_post_schema)
-
-        self.logger.info("Stream status: {0}".format(data["room_status"]))
-        if (data["success"] is True and data["room_status"] == "public" and data["url"]):
-            for s in HLSStream.parse_variant_playlist(self.session, data["url"]).items():
-                yield s
-
-
-__plugin__ = Chaturbate
